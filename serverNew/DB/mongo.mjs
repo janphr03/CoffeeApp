7f4c36428d8112795551992f927c23f1eb261a31
Jan Herrmann <janpppherrmann@gmail.com>
2025-08-06 13:30:37 +0200

    initial commit

---
+++ serverNew/DB/mongo.mjs
@@ -0,0 +1,60 @@
+﻿// Datei: DB/findUser.js
+import { MongoClient } from 'mongodb';
+
+const uri = "mongodb+srv://janpppherrmann:XaTo1ON9ac0ZsGHp@coffeeapp.nxw2owg.mongodb.net/?retryWrites=true&w=majority&appName=CoffeeApp";
+const client = new MongoClient(uri);
+
+const username = "Jan";
+
+export async function getUserData() {
+    try {
+        await client.connect();
+        const db = client.db("CoffeeAppDB"); // <- das ist die DB wie im Screenshot
+        const collection = db.collection("Users");
+
+        const user = await collection.findOne({ name: username });
+
+        if (user) {
+            console.log("✅ Benutzer gefunden:", user);
+        } else {
+            console.log("❌ Kein Benutzer mit dem Namen gefunden.");
+        }
+    } catch (err) {
+        console.error("Fehler beim Abrufen:", err);
+    } finally {
+        await client.close();
+    }
+}
+
+export default { getUserData };
+
+
+
+
+/*import {MongoClient} from "mongodb";
+
+const uri = "mongodb+srv://janpppherrmann:XaTo1ON9ac0ZsGHp@coffeeapp.nxw2owg.mongodb.net/?retryWrites=true&w=majority&appName=CoffeeApp";
+const database = "CoffeeSpots";
+
+
+const client = new MongoClient(uri);
+
+async function run(){
+
+    try {
+        await client.connect()
+
+        const database = database.collection("users");
+        const meinEintrag = users.findOne({name: "Jan"});
+    }
+    catch (error) {
+        console.log(error);
+    }
+    finally {
+        client.close();
+    }
+
+
+    console.log(meinEintrag);
+}*/
+
---
+++ serverNew/server.mjs
@@ -0,0 +1,23 @@
+﻿import express from 'express';
+import mongo from './DB/mongo.mjs';
+
+
+
+const app = express()
+const port = 3000
+
+// Mongo-DB Verbindungsdaten
+app.get('/', async (req, res) => {
+    console.log(await mongo.getUserData())
+    res.send('Hello World!')
+})
+
+// DB-PW: XaTo1ON9ac0ZsGHp
+const uri = "mongodb+srv://janpppherrmann:XaTo1ON9ac0ZsGHp@coffeeapp.nxw2owg.mongodb.net/?retryWrites=true&w=majority&appName=CoffeeApp"
+
+
+
+// startet den Server
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
